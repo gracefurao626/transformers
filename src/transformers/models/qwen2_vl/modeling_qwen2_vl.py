80c6f1f4291c225a120004464a20297bd8eab78d
Rao Fu <61812896+gracefurao626@users.noreply.github.com>
2026-04-02 12:33:53 -0400

    Update modeling_qwen2_vl.py
    Split the concatenated list of audio values for encoding.

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -948,16 +948,18 @@ class Qwen2VLAudioModel(nn.Module):
         self.feature_extractor = WhisperFeatureExtractor.from_pretrained(config.model_name)
         self.encoder = WhisperModel.from_pretrained(config.model_name).encoder
 
-    def forward(self, audio_values):
+    def forward(self, audio_values, audio_lengths):
         # audio_values: list of 1D numpy arrays
+        audio_list = torch.split(audio_values, audio_lengths.tolist())
+        
         # Casting input features → encoder dtype
         features = self.feature_extractor(
-            audio_values,
+            audio_list,
             sampling_rate=self.feature_extractor.sampling_rate,
             return_tensors="pt"
-        ).input_features.to(device=self.encoder.device, dtype=self.encoder.dtype) # [batch, 128, 3000]
+        ).input_features.to(device=self.encoder.device, dtype=self.encoder.dtype) # [batch, n_mels (128), 3000]
 
-        hidden_states = self.encoder(features).last_hidden_state # [batch, 1500, 1280]
+        hidden_states = self.encoder(features).last_hidden_state # [batch, 1500, encoder_dim(1280)]
         return hidden_states
 
 
